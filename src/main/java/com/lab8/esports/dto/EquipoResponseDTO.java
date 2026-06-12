db202f8297658780096508973d589a3d3ad1a9e9
aldrolin05-web <aldrolin05@gmail.com>
2026-06-11 19:15:09 -0500

    Add src/main/java/com/lab8/esports/dto/EquipoResponseDTO.java

---
+++ src/main/java/com/lab8/esports/dto/EquipoResponseDTO.java
@@ -0,0 +1,13 @@
+package com.lab8.esports.dto;
+
+import lombok.Data;
+
+@Data
+public class EquipoResponseDTO {
+    private String nombre;
+    private String tag;
+    private String capitan;
+    private String juego;
+    private String pais;
+    private String estado;
+}
