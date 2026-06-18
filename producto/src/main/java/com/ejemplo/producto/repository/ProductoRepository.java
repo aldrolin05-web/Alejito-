b71e803cb81c85bad75025e132c0f9b2340847d7
aldrolin05-web <aldrolin05@gmail.com>
2026-06-18 18:08:55 -0500

    Remove producto/src/main/java/com/ejemplo/producto/repository/ProductoRepository.java

--- producto/src/main/java/com/ejemplo/producto/repository/ProductoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ejemplo.producto.repository;
-
-import com.ejemplo.producto.model.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
-}
